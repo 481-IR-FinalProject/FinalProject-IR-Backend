73a6cb47b9a44ee93b9cfecc1842bd12ecf234ae
LnRabbiT <blackneko1234@hotmail.com>
2022-03-01 19:23:28 +0700

    Finish clean data generator

---
+++ src/CleanAndGenerateData.py
@@ -0,0 +1,50 @@
+import string
+
+import numpy as np
+import pandas as pd
+
+if __name__ == '__main__':
+    try:
+        file = "resources/Food Ingredients and Recipe Dataset with Image Name Mapping.csv"
+        path = "resources/FoodCleanedData.csv"
+    except:
+        file = "src/resources/Food Ingredients and Recipe Dataset with Image Name Mapping.csv"
+        path = "src/resources/FoodCleanedData.csv"
+    data = pd.read_csv(file, usecols=['Title', 'Instructions', 'Image_Name', 'Cleaned_Ingredients'])
+    data = data.replace('#NAME?', np.nan)
+    data = data.replace('', np.nan)
+    data = data.dropna(how="any")
+    data.drop_duplicates()
+
+    title = data['Title']
+    instruction = data['Instructions']
+    image = data['Image_Name']
+    ingredient = data['Cleaned_Ingredients']
+
+    cleaned_title = title.apply(lambda s: str(s).translate(str.maketrans('', '', string.punctuation + u'\xa0')))
+    cleaned_title = cleaned_title.apply(lambda s: s.lower())
+    cleaned_title = cleaned_title.apply(
+        lambda s: s.translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), '')))
+
+    cleaned_instructions = instruction.apply(
+        lambda s: str(s).translate(str.maketrans('', '', string.punctuation + u'\xa0')))
+    cleaned_instructions = cleaned_instructions.apply(lambda s: s.lower())
+    cleaned_instructions = cleaned_instructions.apply(
+        lambda s: s.translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), '')))
+
+    cleaned_ingredients = ingredient.apply(
+        lambda s: str(s).translate(str.maketrans('', '', string.punctuation + u'\xa0')))
+    cleaned_ingredients = cleaned_ingredients.apply(lambda s: s.lower())
+    cleaned_ingredients = cleaned_ingredients.apply(
+        lambda s: s.translate(str.maketrans(string.whitespace, ' ' * len(string.whitespace), '')))
+
+    allCleaned = {"title": cleaned_title, "instruction": cleaned_instructions, "image": image,
+                  "ingredient": cleaned_ingredients}
+    dataFrame = pd.DataFrame(data=allCleaned)
+    dataFrame.reset_index(inplace=True)
+    del dataFrame["index"]
+    try:
+        dataFrame.to_csv(path, encoding='utf8', index=False)
+        print("✨✨ Generate new CSV successfully ✨✨")
+    except:
+        print("❗❗ Please close the running file and try again ❗❗")
