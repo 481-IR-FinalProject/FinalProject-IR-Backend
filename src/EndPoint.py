ed426993354cfa3db17eda29089e1fe17cd0b787
vector2588 <natkamol082@gmail.com>
2022-03-02 13:15:52 +0700

    Prepare Endpoint for front-end

---
+++ src/EndPoint.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request
+from main import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route('/TF-IDFsearch', methods=['POST'])
+def TF_IDFSearch():
+    return jsonify(TFIDF(request.json['query']))
+
+if __name__ == '__main__':
+    app.run()
