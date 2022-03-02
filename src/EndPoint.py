b8b9e33ee3e212f42c55e549f279d19790a1f857
LnRabbiT <blackneko1234@hotmail.com>
2022-03-02 18:35:01 +0700

    Almost done user authentication

--- src/EndPoint.py
+++ src/EndPoint.py
@@ -2,17 +2,23 @@ from flask import Flask, jsonify, request
 from main import *
 from flask_cors import CORS
 import sqlite3
+import hashlib
+import jwt
 
 app = Flask(__name__)
 app.secret_key = "__privatekey__"
 CORS(app)
 
-
-def __init__(self):
-    con = sqlite3.connect('mydb.db')
-    c = con.cursor()
-    c.execute("CREATE  TABLE user(username text, password text)")
-    con.commit()
+try:
+    con = sqlite3.connect('src/database/mydb.db')
+except:
+    con = sqlite3.connect('database/mydb.db')
+c = con.cursor()
+try:
+    c.execute("CREATE TABLE user(id INTEGER, username text UNIQUE, password text, PRIMARY KEY (id))")
+except:
+    print("Table already exist")
+con.commit()
 
 
 @app.route("/", methods=['POST'])
@@ -22,12 +28,51 @@ def hello_world():
 
 @app.route('/register', methods=['POST'])
 def UserRegister():
-    return "<p>Hello, World!</p>"
+    try:
+        con = sqlite3.connect('src/database/mydb.db')
+    except:
+        con = sqlite3.connect('database/mydb.db')
+    c = con.cursor()
+    if (request.json['username'] != "" and request.json['password'] != ""):
+        username = request.json['username']
+        password = hashlib.md5(request.json['password'].encode()).hexdigest()
+        statement = f"SELECT * from user WHERE username = '{username}' AND password='{password}';"
+        c.execute(statement)
+        data = c.fetchone()
+        if data:
+            return "<p>Error</p>"
+        else:
+            if not data:
+                c.execute("INSERT INTO user (username, password) VALUES (?,?)", (username, password))
+                con.commit()
+                con.close()
+            return "Register successfully!!!"
 
 
 @app.route('/login', methods=['POST'])
 def UserLogin():
-    return "<p>Hello, World!</p>"
+    username = request.json['username']
+    password = hashlib.md5(request.json['password'].encode()).hexdigest()
+    try:
+        con = sqlite3.connect('src/database/mydb.db')
+    except:
+        con = sqlite3.connect('database/mydb.db')
+    c = con.cursor()
+    statement = f"SELECT * from user WHERE username = '{username}' AND password='{password}';"
+    c.execute(statement)
+    data = c.fetchone()
+    encoded_jwt = jwt.encode({"id": data[0], "username": data[1]}, "secret", algorithm="HS256")
+    result = {
+        "token": encoded_jwt,
+        "user": {"username": data[1],
+                 "favorite": [
+                     {"id": 1, "title": "chicken"},
+                     {"id": 2, "title": "butter"},
+                     {"id": 3, "title": "cheese"}
+                 ]
+                 }
+    }
+    return result
 
 
 @app.route('/TF-IDFsearch', methods=['POST'])
