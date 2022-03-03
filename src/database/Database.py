579a9c330e72e463b578653b57afb3290b2199ae
LnRabbiT <blackneko1234@hotmail.com>
2022-03-03 23:44:28 +0700

    Catch the add food

--- src/database/Database.py
+++ src/database/Database.py
@@ -1,38 +1,24 @@
 import csv
-
-import numpy as np
-from flask import Flask
 import sqlite3
-import pandas as pd
 
-# def __init__(self):
-#     con=sqlite3.connect('mydb.db')
-#     c=con.cursor()
-#     c.execute("")
+con = sqlite3.connect("mydb.db")
+cur = con.cursor()
 
 
 def addDataIntoDatabase():
-    con = sqlite3.connect("mydb.db")
-    cur = con.cursor()
-
-    a_file = open("D:/Course/Third year/Second term/Information Retrieval/Project II/FinalProject-IR-Backend/src/resources/FoodCleanedData.csv", encoding='utf-8')
+    a_file = open("../resources/FoodCleanedData.csv",
+                  encoding='utf-8')
     rows = csv.reader(a_file)
     next(rows)
-    cur.executemany("INSERT INTO food(title,ingredient,instruction,image) VALUES ( ?, ?, ?, ?)", rows)
+    cur.executemany("INSERT INTO food(title, ingredient, instruction, image) VALUES (?, ?, ?, ?)", rows)
     con.commit()
     con.close()
 
-def createTable():
-    try:
-        con = sqlite3.connect('src/database/mydb.db')
-    except:
-        con = sqlite3.connect('database/mydb.db')
-    c = con.cursor()
-    try:
-        c.execute("CREATE TABLE user(id INTEGER, username text UNIQUE, password text, PRIMARY KEY (id))")
-    except:
-        print("Table already exist")
-    con.commit()
 
-
-addDataIntoDatabase()
+if __name__ == '__main__':
+    fetchTitleChecker = cur.execute("SELECT title FROM food").fetchall()
+    if (fetchTitleChecker == []):
+        addDataIntoDatabase()
+        print("Add food data to database successfully!!!")
+    else:
+        print("Cannot add food data anymore because food data already exist!!!")
